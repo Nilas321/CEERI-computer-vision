9ae03937334b8382f8b2ca44af9bfecc5efe6a1c
luttapi07 <exclusivelyforproject@gmail.com>
2025-06-05 15:05:18 +0530

    .

--- Recodecv.py
+++ Recodecv.py
@@ -1,12 +1,12 @@
 import cv2
 
-cap = cv2.VideoCapture(0)  # Use camera or video stream
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)  # Use camera or video stream
 
 # Use mp4 codec
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('background.mp4', fourcc, 20.0, (640, 480))
 
-print("Recording background... Press 'q' to stop.")
+print("Recording background... Press 'ENTER KEY' to stop.")
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -16,7 +16,7 @@ while cap.isOpened():
     out.write(frame)
     cv2.imshow('Recording background', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1)==13:
         break
 
 cap.release()
